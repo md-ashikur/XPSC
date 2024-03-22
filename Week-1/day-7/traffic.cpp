20473483d1a9be00e828d8b0a52d84d5c31be37e
md-ashikur <ashik76690@gmail.com>
2024-03-22 16:18:26 +0600

    traffic light error solved

--- Week-1/day-7/traffic.cpp
+++ Week-1/day-7/traffic.cpp
@@ -1,38 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         long long n;
         cin >> n;
-        char c;
-        cin >> c;
+        char ch;
+        cin >> ch;
         string s;
         cin >> s;
-        
-        string ds = s + s; 
-       
+
+        s += s;
+
         vector<long long> v;
-        for (long long i = 0; i < n; i++) {
-            if (s[i] == c) {
+        for (long long i = 0; i < n; i++)
+        {
+            if (s[i] == ch)
+            {
                 v.push_back(i);
             }
         }
 
-       
-        long long maxWait = 0;
-        for (long long index : v) {
-            long long wait = 0;
-            while (ds[index] != 'g') {
-                wait++;
-                index++;
-            }
-            maxWait = max(maxWait, wait);
+        long long ans = LLONG_MIN;
+        long long dis = 0;
+        for (long long i = 2 * n - 2; i >= 0; i--)
+        {
+            if (s[i] == 'g')
+                dis = 0;
+            if (s[i] == ch)
+                ans = max(ans, dis);
+            dis++;
         }
 
-        cout << maxWait << endl;
+        cout << ans << endl;
     }
 
     return 0;

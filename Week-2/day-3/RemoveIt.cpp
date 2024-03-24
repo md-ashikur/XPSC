f5492952efc9e91caffa881ec7fb4e5689da09bc
md-ashikur <ashik76690@gmail.com>
2024-03-24 12:48:21 +0600

    remove it solved

--- Week-2/day-3/RemoveIt.cpp
+++ Week-2/day-3/RemoveIt.cpp
@@ -1,8 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> v(n);
 
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i] != m)
+        {
+            v[i - count] = v[i];
+        }
+        else
+        {
+            count++;
+        }
+    }
+
+    for (int i = 0; i < n - count; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }

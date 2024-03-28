3370ee436916006a51fbe5929d4e74cfe7f45979
md-ashikur <ashik76690@gmail.com>
2024-03-28 20:46:19 +0600

    move bracket

---
+++ Week-2/day-7/MoveBrackets.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+        int count = 0;
+        int count2 = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '(')
+            {
+                count2++;
+            }
+            else
+            {
+                if (count2 == 0)
+                {
+                    count++;
+                }
+                else
+                {
+                    count2--;
+                }
+            }
+        }
+        cout <<count << endl;
+    }
+
+    return 0;
+}

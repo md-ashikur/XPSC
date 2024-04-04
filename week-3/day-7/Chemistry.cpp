8a821be56e24832f64f59842d9bb8fbc7856ccfa
md-ashikur <ashik76690@gmail.com>
2024-04-04 14:15:48 +0600

    chemistry

--- week-3/day-7/Chemistry.cpp
+++ week-3/day-7/Chemistry.cpp
@@ -1,52 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long int
-
 
 int main()
 {
 
-    
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-        int n,k;
-        cin>>n>>k;
+        int n, k;
+        cin >> n >> k;
         string s;
-        cin>>s;
-        vector<int>frq(26,0);
-        for(char c:s)
+        cin >> s;
+
+        map<char, int> mp;
+        for (int i = 0; i < n; i++)
         {
-            frq[c-'a']++;
+            mp[s[i]]++;
         }
-        vector<int>frq2(26,0);
-        for(int i=0;i<26;i++)
+        int count = 0;
+        for (auto c : mp)
         {
-            frq2[frq[i]]++;
-        }
-        sort(frq.begin(),frq.end());
-        while(k!=0)
-        {
-            for(int i=0;i<26;i++)
+            if (c.second % 2 != 0)
             {
-                if(frq[i]!=0)
-                {
-                    frq[i]--;
-                    k--;
-                }
+                count++;
             }
         }
-        bool ans=false;
-        for(int i=0;i<26;i++)
+
+        if (k >= count - 1)
         {
-            if(i%2!=0 && frq2[i]>1)
-            {
-                ans=true;
-            }
+            cout << "YES" << endl;
         }
-        if(ans==true) cout<<"NO"<<'\n';
-        else cout<<"YES"<<'\n';
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }

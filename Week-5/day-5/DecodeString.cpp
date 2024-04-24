693a52f470e47b2faffcfb744d378acd65499d2c
md-ashikur <ashik76690@gmail.com>
2024-04-24 19:01:53 +0600

    day 6

--- Week-5/day-5/DecodeString.cpp
+++ Week-5/day-5/DecodeString.cpp
@@ -9,8 +9,20 @@ int main()
     while (t--)
     {
         int n;
-        cin >> n;
-      
+        string s, ans;
+        cin >> n >> s;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            int x = s[i] - '0';
+            if (x == 0)
+            {
+                x = s[--i] - '0' + (s[--i] - '0') * 10;
+            }
+
+            ans += char(x + 96);
+        }
+        reverse(ans.begin(), ans.end());
+        cout << ans << endl;
     }
     return 0;
 }
